9a6fa86cb885e6eac598c1738ca1754abe4e8c43
burns-edu <ryan.h.burns@edu.sait.ca>
2026-01-20 16:06:05 -0700

    Commit

---
+++ app/week-2/student-info.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link"
+
+export function StudentInfo() {
+  return (
+    <div>
+    <p>
+      Name: Ryan Burns
+    </p>
+    <p>
+      <Link href="https://github.com/burns-edu/cprg306-assignments">
+        GitHub
+      </Link>
+    </p>
+    </div>
+  );
+}
